a0a94da8dd9ec3da3472b6d31d3922cca28745ae
Bruno Lippert <brunohlippert@gmail.com>
2019-03-12 11:16:39 -0300

    first commit

---
+++ Greeter.java
@@ -0,0 +1,5 @@
+class Greeter {
+    public String sayHello() { 
+        return "Hello world!"; 
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import org.joda.time.LocalTime; 
+
+class HelloWorld { 
+    public static void main(String[] args) { 
+        LocalTime currentTime = new LocalTime(); 
+        System.out.println("The current local time is: " + currentTime); 
+        Greeter greeter = new Greeter(); 
+        System.out.println(greeter.sayHello()); 
+    } 
+} 
